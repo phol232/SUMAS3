d1c581a46b1d37551e6e16b00ed08303ffa8c147
phol <72890842@continental.edu.pe>
2025-01-10 17:26:48 -0500

    Se agrego nueva funcionalidad

--- .idea/SUMA4.py
+++ .idea/SUMA4.py
@@ -4,3 +4,6 @@ if __name__ == '__main__':
     primerSumando = int(input("Ingrese el primer sumando: "))
 segundoSumando = int(input("Ingrese segundo sumando: "))
 print(f"{primerSumando} + {segundoSumando} = {suma(primerSumando,segundoSumando)}")
+
+def suma(primerSumando, segundoSumando):
+    return primerSumando + segundoSumando
